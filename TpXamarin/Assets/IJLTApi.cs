d37724bd30b38e387f5146508d2f2ec24638b806
Jules LAURENT <jules.laurent@ynov.com>
2019-01-19 20:09:58 +0100

    add Refit

---
+++ TpXamarin/Assets/IJLTApi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+using TpXamarin.Models;
+
+namespace TpXamarin.JLTApi
+{
+    interface IJLTApi
+    {
+        [Get("/articles")]
+        Task<List<Article>> GetArticles();
+    }
+}
